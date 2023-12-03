1b3c1a5a338696e1408ca949203dc82aa0dc3a25
iovxw <iovxw@outlook.com>
2023-12-03 19:16:53 +0800

    Update keymap

--- firmware/kmk/main.py
+++ firmware/kmk/main.py
@@ -35,12 +35,12 @@ keyboard.modules.append(RGBLayers())
 keyboard.keymap = [
     [
         KC.NO,   KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,  KC.NO,
-    KC.NO,   KC.LEFT, KC.DOWN, KC.UP,   KC.RGHT, KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.DEL,
+    KC.NO,   KC.LEFT, KC.DOWN, KC.UP,   KC.RGHT, KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.HOME, KC.PGDN, KC.PGUP, KC.END,  KC.DEL,
         KC.GRV,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.MINS, KC.EQL,  KC.BSPC,
     KC.PGUP, KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.LBRC, KC.RBRC, KC.BSLS,
         KC.PGDN,   KC.CAPS, KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, KC.QUOT, KC.ENT,
-    KC.HOME, KC.END,  KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.SLSH, KC.RSFT, KC.NO,
-                          KC.TT(1),           KC.ESC,           KC.LALT, KC.NO,            KC.BSPC,          KC.TT(2),
+    KC.HOME, KC.END,  KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.SLSH, KC.RSFT, KC.PSCR,
+                          KC.TT(1),           KC.ESC,           KC.NO,   KC.LALT,          KC.BSPC,          KC.TT(2),
                                                  KC.LSFT,          KC.LGUI,          KC.LCTL,
                                                      KC.SPC,                    KC.ENT,
     ],[
